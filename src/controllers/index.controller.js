11dc1399fe01198bfe24d850d812e63aa35ef09d
MiguelJRM95 <miguelitoworkout@gmail.com>
2021-05-31 01:29:00 +0200

    Home with button counter

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -3,8 +3,15 @@ import viewHome from '../views/home.html';
 export default () => {
    
     let template = document.createElement('div');
-    template.classList = 'text-danger';
+    template.classList = 'text-dark';
     template.innerHTML = viewHome;
+    const btn = template.querySelector('#btnClick');
+    let counter = template.querySelector('#counterP');
+    let clicks = 0;
+    btn.addEventListener('click', ()=>{
+        clicks += 1;
+        counter.innerHTML= clicks;
+    })
 
     return template;
 }
