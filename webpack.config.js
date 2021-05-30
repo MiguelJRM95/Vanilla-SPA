9621cb538a1adf152b00f4470fa96dc5a2640177
MiguelJRM95 <miguelitoworkout@gmail.com>
2021-05-30 23:50:08 +0200

    html plugin for webpack added

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/main.js',
     output: {
         path: __dirname + '/dist',
         filename: 'app.js'
-    }
+    },
+    mode: 'development',
+    plugins: [
+        new HTMLWebpackPlugin()
+    ]
 }
